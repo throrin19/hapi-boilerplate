5020a45089198fb2a8692fc5716c82c24bb27b4b
Benjamin Besse <benjamin.besse02@gmail.com>
2018-01-29 18:19:46 +0100

    remove unused dependencies

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 
 const manifest = require('./config/manifest');
 
-manifest.init().then(server => {
-    server.start(err => {
+manifest.init().then((server) => {
+    server.start((err) => {
         if (err) {
             throw err;
         }
 
         console.log(`Server running at: ${server.info.uri}`);
     });
-}).catch(err => {
+}).catch((err) => {
     throw err;
 });
